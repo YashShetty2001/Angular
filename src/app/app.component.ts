281925d51fa9e9062f8f3b21dc6a1cb566cd67b8
YashShetty2001 <yashshetty2024@gmail.com>
2023-09-20 22:43:03 +0530

    For Loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Switch Case';
-  color='blue';
+  title = 'For Loop';
+  users=['ram', 'shyam','lakshman','bharat', 'shatrungan']
+  Userlist=[
+  {name:'ram', email:'ram@test.com', phone:'8888'},
+  {name:'shyam', email:'shyam@test.com', phone:'2222'},
+  {name:'lakshman', email:'lakshman@test.com', phone:'3333'},
+  {name:'bharat', email:'bharat@test.com', phone:'4444'},
+  {name:'shatrungan', email:'shatrungan@test.com', phone:'2222'}
+  ]
 }
