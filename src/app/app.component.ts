c0c88ee86fadbf849318f699feea0e0ec8656e2e
YashShetty2001 <yashshetty2024@gmail.com>
2023-09-21 00:12:41 +0530

    Nested for loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'For Loop';
+  title = 'Nested For Loop';
   users=['ram', 'shyam','lakshman','bharat', 'shatrungan']
   Userlist=[
-  {name:'ram', email:'ram@test.com', phone:'8888'},
-  {name:'shyam', email:'shyam@test.com', phone:'2222'},
-  {name:'lakshman', email:'lakshman@test.com', phone:'3333'},
-  {name:'bharat', email:'bharat@test.com', phone:'4444'},
-  {name:'shatrungan', email:'shatrungan@test.com', phone:'2222'}
+  {name:'ram', email:'ram@test.com', phone:'8888', SocialAccounts:['gmail','insta','facebook','youtube']},
+  {name:'shyam', email:'shyam@test.com', phone:'2222', SocialAccounts:['gmail','youtube','insta','facebook','youtube']},
+  {name:'lakshman', email:'lakshman@test.com', phone:'3333', SocialAccounts:['yahoo','insta','facebook']},
+  {name:'bharat', email:'bharat@test.com', phone:'4444', SocialAccounts:['gmail','insta','facebook','youtube']},
+  {name:'shatrungan', email:'shatrungan@test.com', phone:'2222', SocialAccounts:['gmail','facebook','youtube']}
   ]
 }
