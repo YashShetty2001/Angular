c1c568665847e58b149813bd0e1349652caa2c72
YashShetty2001 <yashshetty2024@gmail.com>
2023-09-21 15:15:59 +0530

    Basic forms in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import {FormsModule} from '@angular/forms';
 
 
 
@@ -10,14 +10,14 @@ import { HeaderComponent } from './header/header.component';
 @NgModule({
     declarations: [
         AppComponent,
-        HeaderComponent,
+
  
     ],
     providers: [],
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
-        
+        FormsModule
     ]
 })
 export class AppModule { }
