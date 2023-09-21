43e72525ffa813e3af5b2d67bb62d951c7f0bfc3
YashShetty2001 <yashshetty2024@gmail.com>
2023-09-21 14:45:08 +0530

    Header in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Style Binding';
-  color='red';
-  bgcolor='purple';
-  counter:number=1;
-  UpdateColor(){
-    if(this.counter%2==0)
-    {
-    this.color='blue';
-    this.bgcolor='yellow';
-    }
-    else{
-      this.color='green';
-      this.bgcolor='orange';
-    }
-    this.counter++;
-  }
+  title = 'Header in Angular';
+ 
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
 @NgModule({
     declarations: [
         AppComponent,
+        HeaderComponent,
  
     ],
     providers: [],
