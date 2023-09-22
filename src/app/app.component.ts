2f09f73186ab214f6801407753c12b85dd727e18
YashShetty2001 <yashshetty2024@gmail.com>
2023-09-23 00:34:52 +0530

    Sending Data from Parent component to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Todo List with Angular';
-  list:any[]=[];
-  addTask(item:string)
+  title = 'Sending Data from child component to parent compononent';
+  data=10;
+  updateChild()
   {
-    
-    this.list.push({id:this.list.length,name:item});
-    console.warn(this.list)
-  }
-  removeTask(id:number)
-  {
-    console.warn(id);
-    this.list=this.list.filter(item=>item.id!=id);
+    this.data=Math.floor(Math.random()*10);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,12 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
 import {MatBadgeModule} from '@angular/material/badge';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
