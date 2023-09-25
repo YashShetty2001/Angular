7cd6c601b0971f6ab832ea19f2a9387330da1938
YashShetty2001 <yashshetty2024@gmail.com>
2023-09-25 23:29:30 +0530

    Validation in Reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,32 @@
 import { Component } from '@angular/core';
-import{FormControl,FormGroup} from '@angular/forms'
+import{FormControl,FormGroup,Validator, Validators} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Reactive form in Angular';
+  title = 'Validation in Reactive form';
   loginForm=new FormGroup({
-    name: new FormControl(''),
-    email:new FormControl(''),
-    password:new FormControl('')
+    name: new FormControl('',Validators.required),
+    email:new FormControl('',[Validators.email,Validators.required]),
+    password:new FormControl('',[Validators.required,Validators.minLength(5),Validators.pattern('[a-zA-z]+$')])
 
   })
 loginUser(){
   console.warn(this.loginForm.value);
 }
 
+get user()
+{
+  return this.loginForm.get('name');
+}
+get email()
+{
+   return this.loginForm.get('email')
+}
+get password()
+{
+  return this.loginForm.get('password')
+}
 }
