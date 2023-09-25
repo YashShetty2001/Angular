0ebc5a7f008bbe6def034be16944384b42185627
YashShetty2001 <yashshetty2024@gmail.com>
2023-09-26 00:04:15 +0530

    Directives in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,6 @@ import{FormControl,FormGroup,Validator, Validators} from '@angular/forms'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Validation in Reactive form';
-  loginForm=new FormGroup({
-    name: new FormControl('',Validators.required),
-    email:new FormControl('',[Validators.email,Validators.required]),
-    password:new FormControl('',[Validators.required,Validators.minLength(5),Validators.pattern('[a-zA-z]+$')])
+  title = 'Directives in Angular';
 
-  })
-loginUser(){
-  console.warn(this.loginForm.value);
-}
-
-get user()
-{
-  return this.loginForm.get('name');
-}
-get email()
-{
-   return this.loginForm.get('email')
-}
-get password()
-{
-  return this.loginForm.get('password')
-}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FormsModule } from '@angular/forms';
-import {ReactiveFormsModule} from '@angular/forms'
+import {ReactiveFormsModule} from '@angular/forms';
+import { RedELDirective } from './red-el.directive'
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    RedELDirective,
    
 
   
